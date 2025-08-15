83c93e5588e4bec5767c8cfefb11b3be90af251a
Yellapu MeghaShyam <22l61a6118@cec.ac.in>
2025-08-15 22:38:55 +0530

    Time: 1 ms (53.01%) | Memory: 17.9 MB (10.22%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -9,10 +9,7 @@ class Solution:
             if r-l==3:
                 temp.remove(s[l])
                 l+=1
-            if r-l+1==3:
-                if temp[0]!=temp[1]:
-                    if temp[1]!=temp[2]:
-                        if temp[2]!=temp[0]:
-                            ans+=1
+            if r-l+1==3 and len(set(temp))==3:
+                ans+=1
         return ans
         
