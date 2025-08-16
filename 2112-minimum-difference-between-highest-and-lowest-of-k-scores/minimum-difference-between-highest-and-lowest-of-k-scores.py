fe3d67cb456c57a7980cd2b1e4c45835d74b2d4d
Yellapu MeghaShyam <22l61a6118@cec.ac.in>
2025-08-16 11:57:48 +0530

    Time: 11 ms (33.23%) | Memory: 17.8 MB (90.24%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -7,9 +7,9 @@ class Solution:
         l=0
         for r in range(n):
             temp.append(nums[r])
-            if (r-l==k):
-                l+=1
+            # if (r-l==k):
+            #     l+=1
             if r-l+1==k:
                 ans=min(ans,temp[r]-temp[l])
-                # l+=1
+                l+=1
         return ans
