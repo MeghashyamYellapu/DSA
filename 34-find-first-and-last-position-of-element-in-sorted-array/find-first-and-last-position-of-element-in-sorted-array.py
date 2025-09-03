cd44af1c033078f4839f557b5118e61f15e878e3
Yellapu MeghaShyam <22l61a6118@cec.ac.in>
2025-09-03 21:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (20.08%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,47 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         ans=[-1,-1]
-        if len(nums)==1 and target==nums[0]:
-            return [0,0]
-        def left (nums,target):
-            l=0
-            r=len(nums)-1
-            while(l<=r):
-                mid=int(l+(r-l)/2)
-                
-                if nums[mid]>=target:
-                    r=mid-1
-                else :
-                    l=mid+1
-            print(l)
-            if (l<0 or l>=len(nums))  or nums[l]!=target :
-                return -1
-            elif nums[l]==target:
-                return l
-            else :
-                return -1
-        def right (nums,target):
+        def right(nums,target):
             l=0
             r=len(nums)-1
             while(l<=r):
                 mid=int(l+(r-l)/2)
                 if nums[mid]>target:
                     r=mid-1
-                else :
+                else:
                     l=mid+1
-                print(ans,mid)
-            
-            if (r<0 or r>=len(nums))  or nums[r]!=target :
+            if (r<0 or r>=len(nums)) or nums[r]!=target:
                 return -1
-            elif nums[r]==target:
-                return r
-            else :
+            return r
+        def left(nums,target):
+            l=0
+            r=len(nums)-1
+            while(l<=r):
+                mid=int(l+(r-l)/2)
+                if nums[mid]<target:
+                    l=mid+1
+                else:
+                    r=mid-1
+            if (l<0 or l>=len(nums)) or nums[l]!=target:
                 return -1
-        
-        ans[1]=right(nums,target)
+            return l
         ans[0]=left(nums,target)
-        # if ans[0]=='':
-        #     ans[0]=ans[1]
-        # ans.sort()
-          
+        ans[1]=right(nums,target)
+        print(ans)
         return ans
